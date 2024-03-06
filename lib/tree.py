b53b2335d6636f58a214618738fff5dae11b52e5
Charlie Newlinds <cnewlinds@gmail.com>
2024-03-06 17:58:00 +1100

    test pass

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,18 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    if self.root is None:
+      return None
+    return self.depth_first_traversal(self.root, id)
+     
+
+  def depth_first_traversal(self, node, target_id):
+        # Visit each node
+        if node['id'] == target_id:
+            return node
+        for child in node['children']:
+            # Visit each child node
+            result = self.depth_first_traversal(child, target_id)
+            if result:
+                return result
+        return None
